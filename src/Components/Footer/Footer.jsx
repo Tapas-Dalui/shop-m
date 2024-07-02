7815fc2af2334baa0a9ab8dbecb9118dc7e5cf79
Tapas <tdalui865@gmail.com>
2024-07-02 12:38:08 +0530

    change shop name

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../../assets/Shop.png'
+import footer_logo from '../../assets/shop.png'
 import { FaWhatsapp } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
